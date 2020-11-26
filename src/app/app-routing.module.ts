e85ecdfbe522b2b7c7156d2dc105c9ded1681bcf
ghayethbouroui <ghayethbouraoui@gmail.com>
2020-11-26 16:12:26 +0000

    Creating Navbar,footer,home layout's component

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppRoutingModule { }
