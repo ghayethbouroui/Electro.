5d5418fe4e3c107590ac8fccc397c858a8880eb9
ghayethbouroui <ghayethbouraoui@gmail.com>
2020-11-27 11:29:27 +0000

    Creating register and login layout's component + adding routing system

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,32 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import {RegisterComponent} from './register/register.component';
+import {RouterModule} from '@angular/router';
+import { LoginComponent } from './login/login.component';
+const routes: Routes = [
+  // Define routes for the landing / home page, create a separate component for the layout of home page
+  // put only header, footer and router-outlet there
 
+      {
+        path: '', component: HomeComponent
+      },
+      
+      {
+        path: 'register', component: RegisterComponent
+      },
+      {
+        path: 'login', component: LoginComponent
+      },
+    ]
+  
+  
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './app-routing.module';
+import { RegisterComponent } from './register/register.component';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
@@ -14,11 +18,17 @@ import { AppRoutingModule } from './app-routing.module';
     HomeComponent,
     NavbarComponent,
     FooterComponent,
-    ContactComponent
+    ContactComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    RouterModule,  
+    AppRoutingModule,
+    ReactiveFormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
