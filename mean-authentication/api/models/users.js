f11bbad01b6aaa5fd2e784bd0335992b10806e21
houssemabidi94 <houcemabidi1994@gmail.com>
2020-12-19 14:35:35 +0100

    last commit

--- mean-authentication/api/models/users.js
+++ mean-authentication/api/models/users.js
@@ -1,8 +1,7 @@
-const mongoose = require('mongoose');
+
+const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const User = mongoose.model('User');
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -18,61 +17,32 @@ const userSchema = new mongoose.Schema({
   salt: String
 });
 
-mongoose.model('User', userSchema);
-const crypto = require('crypto');
 userSchema.methods.setPassword = function(password) {
-    this.salt = crypto.randomBytes(16).toString('hex');
-    this.hash = crypto
-      .pbkdf2Sync(password, this.salt, 1000, 64, 'sha512')
-      .toString('hex');
-  };
-  userSchema.methods.validPassword = function(password) {
-    const hash = crypto
-      .pbkdf2Sync(password, this.salt, 1000, 64, 'sha512')
-      .toString('hex');
-    return this.hash === hash;
-  };
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto
+    .pbkdf2Sync(password, this.salt, 1000, 64, 'sha512')
+    .toString('hex');
+};
 
-  userSchema.methods.generateJwt = function() {
-    const expiry = new Date();
-    expiry.setDate(expiry.getDate() + 7);
-  
-    return jwt.sign(
-      {
-        _id: this._id,
-        email: this.email,
-        name: this.name,
-        exp: parseInt(expiry.getTime() / 1000)
-      },
-      'MY_SECRET'
-    ); // DO NOT KEEP YOUR SECRET IN THE CODE!
-  };
-  passport.use(
-    new LocalStrategy(
-      {
-        usernameField: 'email'
-      },
-      function(username, password, done) {
-        User.findOne({ email: username }, function(err, user) {
-          if (err) {
-            return done(err);
-          }
-          // Return if user not found in database
-          if (!user) {
-            return done(null, false, {
-              message: 'User not found'
-            });
-          }
-          // Return if password is wrong
-          if (!user.validPassword(password)) {
-            return done(null, false, {
-              message: 'Password is wrong'
-            });
-          }
-          // If credentials are correct, return the user object
-          return done(null, user);
-        });
-      }
-    )
-  );
-  
+userSchema.methods.validPassword = function(password) {
+  const hash = crypto
+    .pbkdf2Sync(password, this.salt, 1000, 64, 'sha512')
+    .toString('hex');
+  return this.hash === hash;
+};
+
+userSchema.methods.generateJwt = function() {
+  const expiry = new Date();
+  expiry.setDate(expiry.getDate() + 7);
+
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      name: this.name,
+      exp: parseInt(expiry.getTime() / 1000)
+    },
+  ); // DO NOT KEEP YOUR SECRET IN THE CODE!
+};
+
+mongoose.model('User', userSchema);
--- mean-authentication/api/routes/index.js
+++ mean-authentication/api/routes/index.js
@@ -1,19 +1,19 @@
 const ctrlAuth = require('../controllers/authentication');
 const ctrlProfile = require('../controllers/profile');
-const jwt = require('express-jwt');
-
-const auth = jwt({
-  secret: 'MY_SECRET',
-  userProperty: 'payload'
-});
-router.get('/profile', auth, ctrlProfile.profileRead);
-
-
 const express = require('express');
+const jwt = require('express-jwt');
 const router = express.Router();
 
+const auth = jwt({
+  secret: 'secret',
+  algorithms: ['HS256'],
+  userProperty: 'payload'
+});
+
+
+
 // profile
-router.get('/profile/:userid', ctrlProfile.profileRead);
+router.get('/profile', auth, ctrlProfile.profileRead);
 
 // authentication
 router.post('/register', ctrlAuth.register);
